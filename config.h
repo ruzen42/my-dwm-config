096fdecae3bad23a26ef4c748d5abb467b57a5fe
ruzen42 <malledeferlive@gmail.com>
2025-04-13 09:46:54 +0500

    add patches

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "terminus:size=12" };
 static const char dmenufont[]       = "terminus:size=12";
 static const char col_gray1[]       = "#5D6B66";
@@ -17,6 +17,13 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+
+static const int showsystray = 1;
+static const int systrayonleft = 1;
+static const int systrayspacing = 1;
+static const int systraypinning = 1;
+static const int systraypinningfailfirst = 1;
+
 static int enablegaps = 1;
 static int gappih = 5; /* horizontal inner gap between windows */
 static int gappiv = 8; /* vertical inner gap between windows */
